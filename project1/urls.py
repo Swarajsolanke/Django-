aee800a1f683b8e832529a13246eaf1ee80e8a81
Swarajsolanke <swarajsolanke02@gmail.com>
2025-04-16 16:47:09 +0530

    new app

--- project1/urls.py
+++ project1/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app1 import views
+from app2 import views  as views2   
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name='home'),
     path("index/",views.index,name="index"),
     path("learn/",views.learn,name='learn'),
-    
-
+    path("index2/",views2.index2,name="index2"),
 ]
