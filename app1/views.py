980dddeb9bb4244b3ac5bf89456bf715e811839d
Swarajsolanke <swarajsolanke02@gmail.com>
2025-04-21 17:21:52 +0530

    url patterns

--- app1/views.py
+++ app1/views.py
@@ -8,8 +8,15 @@ def home(request):
 def index(request):
     return HttpResponse("hello")
 
-def learn(request):
-    return HttpResponse("<h1>hey there</h1>")
+def learn(request,**kwargs):
+    status=kwargs.get("status")
+    print(f"this is the value of key argument: {status}")
+    return HttpResponse(f"<h1>hey there {status}</h1>")
+def study(req, **kwargs):
+    status=kwargs.get('status',"not allowed")
+    print(f"this is the study matreial end point:{status}")
+    return HttpResponse(f"wow you made it:{status}")
+
 
 
 
